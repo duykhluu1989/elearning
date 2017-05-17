6236dd192a4820a61ddf0fd00b7d017662cd18b8
Duy Luu <duykhluu1989@gmail.com>
2017-05-17 16:19:21 +0700

    Widget model

--- app/Models/Widget.php
+++ app/Models/Widget.php
@@ -1,7 +1,102 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: PeChick
- * Date: 17/5/2017
- * Time: 11:14 AM
- */
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Widget extends Model
+{
+    const HOME_SLIDER = 'HOME_SLIDER';
+
+    const STATUS_ACTIVE_DB = 1;
+    const STATUS_INACTIVE_DB = 0;
+    const STATUS_ACTIVE_LABEL = 'Kích Hoạt';
+    const STATUS_INACTIVE_LABEL = 'Vô Hiệu';
+
+    const TYPE_SLIDER_DB = 0;
+    const TYPE_SLIDER_LABEL = 'Khung Ảnh Trượt';
+
+    const ATTRIBUTE_TYPE_STRING_DB = 0;
+    const ATTRIBUTE_TYPE_INT_DB = 1;
+    const ATTRIBUTE_TYPE_JSON_DB = 2;
+    const ATTRIBUTE_TYPE_IMAGE_DB = 3;
+
+    protected $table = 'widget';
+
+    public $timestamps = false;
+
+    public static function initCoreWidgets()
+    {
+        $coreWidgets = [
+            [
+                self::HOME_SLIDER,
+                'Khung Ảnh Trượt Trang Chủ',
+                self::STATUS_ACTIVE_DB,
+                self::TYPE_SLIDER_DB,
+                json_encode([
+                    [
+                        'title' => 'Ảnh',
+                        'name' => 'image',
+                        'type' => self::ATTRIBUTE_TYPE_IMAGE_DB,
+                    ],
+                    [
+                        'title' => 'Tiêu Đề',
+                        'name' => 'title',
+                        'type' => self::ATTRIBUTE_TYPE_STRING_DB,
+                    ],
+                    [
+                        'title' => 'Mô Tả',
+                        'name' => 'description',
+                        'type' => self::ATTRIBUTE_TYPE_STRING_DB,
+                    ],
+                    [
+                        'title' => 'Đường Dẫn',
+                        'name' => 'url',
+                        'type' => self::ATTRIBUTE_TYPE_STRING_DB,
+                    ],
+                    [
+                        'title' => 'Thứ Tự',
+                        'name' => 'order',
+                        'type' => self::ATTRIBUTE_TYPE_INT_DB,
+                    ],
+                ]),
+            ],
+        ];
+
+        foreach($coreWidgets as $coreWidget)
+        {
+            $widget = new Widget();
+            $widget->code = $coreWidget[0];
+            $widget->name = $coreWidget[1];
+            $widget->status = $coreWidget[2];
+            $widget->type = $coreWidget[3];
+            $widget->attribute = $coreWidget[4];
+            $widget->save();
+        }
+    }
+
+    public static function getWidgetStatus($value = null)
+    {
+        $status = [
+            self::STATUS_ACTIVE_DB => self::STATUS_ACTIVE_LABEL,
+            self::STATUS_INACTIVE_DB => self::STATUS_INACTIVE_LABEL,
+        ];
+
+        if($value !== null && isset($status[$value]))
+            return $status[$value];
+
+        return $status;
+    }
+
+    public static function getWidgetType($value = null)
+    {
+        $type = [
+            self::TYPE_SLIDER_DB => self::TYPE_SLIDER_LABEL,
+        ];
+
+        if($value !== null && isset($type[$value]))
+            return $type[$value];
+
+        return $type;
+    }
+}
