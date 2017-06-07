257f56846c0990d14a02ab79d29ce07cfd483908
Duy Luu <duykhluu1989@gmail.com>
2017-06-07 15:22:55 +0700

    Init collaborator settting

--- app/Models/Collaborator.php
+++ app/Models/Collaborator.php
@@ -9,7 +9,10 @@ class Collaborator extends Model
     const COUPON_ATTRIBUTE = 'coupon';
     const COMMISSION_ATTRIBUTE = 'commission';
     const REVENUE_ATTRIBUTE = 'revenue';
+    const RERANK_TIME_ATTRIBUTE = 'rerank_time';
     const COMMISSION_DOWNLINE_ATTRIBUTE = 'commission_downline';
+    const COUPON_DOWNLINE_SET_ATTRIBUTE = 'coupon_downline_set';
+    const COMMISSION_DOWNLINE_SET_ATTRIBUTE = 'commission_downline_set';
 
     protected $table = 'collaborator';
 
